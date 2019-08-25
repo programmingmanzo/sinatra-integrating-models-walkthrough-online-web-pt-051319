3f5f8963ccde1da23b08b1057f87c4171a479f2e
Gerardo Manzo <programmingmanzo@gmail.com>
2019-08-25 20:58:56 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,10 +6,15 @@ class App < Sinatra::Base
     erb :index
   end
 
+=begin
   post '/' do
     text_from_user = params[:user_text]
     
     @analyzed_text = TextAnalyzer.new(text_from_user)
+=end
+    
+  post '/' do #We shortened the above to this below:
+  @analyzed_text = TextAnalyzer.new(params[:user_text])
 
     erb :results
   end
